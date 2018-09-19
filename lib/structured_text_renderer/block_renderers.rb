50d3b30951660a71b6419237ed5c9b65923e1f06
David Litvak <david.litvakb@gmail.com>
2018-09-19 10:57:39 +0200

    Add Headings

--- lib/structured_text_renderer/block_renderers.rb
+++ lib/structured_text_renderer/block_renderers.rb
@@ -1,4 +1,8 @@
 require_relative './block_renderers/entry_block_renderer'
 require_relative './block_renderers/heading_one_renderer'
 require_relative './block_renderers/heading_two_renderer'
+require_relative './block_renderers/heading_three_renderer'
+require_relative './block_renderers/heading_four_renderer'
+require_relative './block_renderers/heading_five_renderer'
+require_relative './block_renderers/heading_six_renderer'
 require_relative './block_renderers/paragraph_renderer'
