d35d49cf72ca1f4385f0a8e8d146a4819a630839
David Litvak <david.litvakb@gmail.com>
2018-09-26 16:49:23 +0200

    Fix rendering logic for block type nodes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # CHANGELOG
 
+## Unreleased
+
+### Fixed
+* Fixed rendering logic for block type nodes
+
 ## v0.0.1
 
 * Initial Release
--- lib/structured_text_renderer/block_renderers/hyperlink_renderer.rb
+++ lib/structured_text_renderer/block_renderers/hyperlink_renderer.rb
@@ -5,11 +5,7 @@ module StructuredTextRenderer
   class HyperlinkRenderer < BaseBlockRenderer
     # Renders hyperlink
     def render(node)
-      content = node['content'].each_with_object([]) do |content_node, result|
-        renderer = find_renderer(content_node)
-        result << renderer.render(content_node)
-      end.join
-      "<a href=\"#{node['data']['uri']}\">#{content}</a>"
+      "<a href=\"#{node['data']['uri']}\">#{render_content(node)}</a>"
     end
   end
 end
